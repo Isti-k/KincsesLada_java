58be8fdf8fd3c43a85caa79e24b6dd014cef3e8d
Kovács István Gábor (SZF_2023_2025) <kovacs.istvan.gabor@diak.szamalk-szalezi.hu>
2024-09-11 15:07:20 +0200

    lada

--- src/kincseslada/KincsesLada.java
+++ src/kincseslada/KincsesLada.java
@@ -4,17 +4,12 @@
  */
 package kincseslada;
 
-/**
- *
- * @author KovácsIstvánGábor(SZ
- */
+
 public class KincsesLada {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
